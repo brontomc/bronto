7a3a0af3e18e2b9ca15198b2b64e6cb7840619c9
icrayix <icrayix@gmail.com>
2024-07-29 20:29:05 +0200

    Remove api

--- agent/agent.go
+++ agent/agent.go
@@ -1,16 +1,4 @@
 package agent
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/brontomc/bronto/agent/api"
-)
-
 func Start() {
-	router := api.SetupRouter()
-
-	port := ":17771"
-	log.Printf("Server starting on %s", port[1:])
-	log.Fatal(http.ListenAndServe(port, router))
 }
--- agent/api/handlers.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func ExampleHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello API!"))
-}
